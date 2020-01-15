d02bb5b34cf4dc5dd71dc582c82efababb37132a
Yannick Knapp <YaBaSa@web.de>
2020-01-15 08:23:40 +0000

    Add files

---
+++ lidar2bev.py
@@ -0,0 +1,156 @@
+import os
+import sys
+import cv2
+import utils
+import imageio
+import numpy as np
+from nuscenes.utils.data_classes import LidarPointCloud
+from scipy.spatial.transform import Rotation as R
+
+# Paths
+KITTI_LIDAR_DIR = "/home/user/datasets/kitti/kitti/object/training/velodyne"
+KITTI_OUTPUT_DIR = "/home/user/datasets/bev_images/kitti/training"
+LYFT_LIDAR_DIR = "/home/user/datasets/lyft_level_5/v1.02-train/lidar"
+LYFT_OUTPUT_DIR = "/home/user/datasets/bev_images/lyft"
+NUSCENES_LIDAR_DIR = "/home/user/datasets/nuscenes/samples/LIDAR_TOP"
+NUSCENES_OUTPUT_DIR = "/home/user/datasets/bev_images/nuscenes/samples"
+
+# lidar boundarys for Bird's Eye View
+BOUNDARY = {
+    "minX": 0,
+    "maxX": 50,
+    "minY": -25,
+    "maxY": 25,
+    "minZ": -2.73,
+    "maxZ": 1.27
+}
+
+
+def load_lidar_file_kitti(file_path):
+    n_vec = 4
+    dtype = np.float32
+    lidar_pc_raw = np.fromfile(file_path, dtype)
+    return lidar_pc_raw.reshape((-1, n_vec))
+
+
+def load_lidar_file_lyft(file_path):
+    n_vec = 5
+    dtype = np.float32
+    lidar_pc_raw = np.fromfile(file_path, dtype)
+    lidar_pc = lidar_pc_raw.reshape((-1, n_vec))
+    intensity_normalized = lidar_pc[:, 3] / 100
+    lidar_pc[:, 3] = intensity_normalized
+    print(lidar_pc.shape)
+    return lidar_pc
+
+
+def load_lidar_file_nuscenes(file_path):
+    n_vec = 5
+    dtype = np.float32
+    lidar_pc_raw = np.fromfile(file_path, dtype)
+    lidar_pc = lidar_pc_raw.reshape((-1, n_vec))
+    #intensity_normalized = lidar_pc[:, 3] / 255
+    #lidar_pc[:, 3] = intensity_normalized
+    pointcloud = LidarPointCloud.from_file(file_path)
+    r = R.from_quat([0, 0, np.sin((np.pi / 4)*3), np.cos((np.pi / 4)*3)])
+    pointcloud.rotate(r.as_matrix())
+    lidar_pc[:, 0] = pointcloud.points[0, :]
+    lidar_pc[:, 1] = pointcloud.points[1, :]
+    lidar_pc[:, 2] = pointcloud.points[2, :]
+    lidar_pc[:, 3] = pointcloud.points[3, :]
+
+    return lidar_pc
+
+
+def check_identity_of_img(filename, lidar_bev):
+    # compare stored matrix with actual matrix
+    print("Check stored file for identity")
+    lidar_bev_file = imageio.imread(filename)
+
+    if np.array_equal(lidar_bev, lidar_bev_file) is False:
+        print("Error: BEV image '%s' is corrupted" % filename)
+        sys.exit()
+    else:
+        print("Identity test successful!")
+
+
+def check_identity_of_img_float(filename, lidar_bev):
+    # compare stored matrix with actual matrix
+    # this method is for comparing matrix with file, which was automatically rounded from float64 to uint8 by imageio
+    print("Check stored file for identity")
+    lidar_bev_file = imageio.imread(filename)
+
+    lidar_bev_round = np.rint((lidar_bev * 255)).astype(np.uint8)
+
+    np.set_printoptions(threshold=sys.maxsize)
+    indices = np.argwhere(lidar_bev_file != lidar_bev_round)
+    for index in indices:
+        print(lidar_bev_file[index[0], index[1], index[2]])
+        print(lidar_bev_round[index[0], index[1], index[2]])
+        print(lidar_bev[index[0], index[1], index[2]] * 255)
+
+    if np.array_equal(lidar_bev_round, lidar_bev_file) is False:
+        print("Error: BEV image '%s' is corrupted" % filename)
+        sys.exit()
+    else:
+        print("Identity test successful!")
+
+
+def main(chosen_dataset, img_height, img_width):
+    load_lidar_file = None
+    lidar_path = None
+    output_dir = None
+
+    if chosen_dataset == "kitti":
+        load_lidar_file = load_lidar_file_kitti
+        lidar_path = KITTI_LIDAR_DIR
+        output_dir = KITTI_OUTPUT_DIR
+    elif chosen_dataset == "lyft":
+        load_lidar_file = load_lidar_file_lyft
+        lidar_path = LYFT_LIDAR_DIR
+        output_dir = LYFT_OUTPUT_DIR
+    elif chosen_dataset == "nuscenes":
+        load_lidar_file = load_lidar_file_nuscenes
+        lidar_path = NUSCENES_LIDAR_DIR
+        output_dir = NUSCENES_OUTPUT_DIR
+    else:
+        print("Error: Unknown dataset '%s'" % chosen_dataset)
+        sys.exit()
+
+    # setup
+    lidar_pc_filenames = os.listdir(lidar_path)
+    num_lidar_pc = len(lidar_pc_filenames)
+
+    for idx in range(num_lidar_pc):
+        print("Processing file '%s' (%s|%s)" % (lidar_pc_filenames[idx], idx+1, num_lidar_pc))
+        file_path = os.path.join(lidar_path, lidar_pc_filenames[idx])
+        lidar_pc = load_lidar_file(file_path=file_path)
+
+        # filter point cloud points inside fov
+        lidar_pc_filtered = utils.removePoints(lidar_pc, BOUNDARY)
+
+        # create Bird's Eye View
+        discretization = (BOUNDARY["maxX"] - BOUNDARY["minX"]) / img_height
+        lidar_bev = utils.makeBVFeature(lidar_pc_filtered, BOUNDARY, img_height, img_width, discretization)
+        ########################
+        # set intensity to zero since lyft doesn't provide intensity values
+        lidar_bev[:, :, 2] = 0.0
+        ########################
+
+        # save as PNG image with the same image name like lidar file
+        output_filename = lidar_pc_filenames[idx].replace(".bin", ".png")
+        output_filepath = os.path.join(output_dir, output_filename)
+        imageio.imwrite(output_filepath, lidar_bev)
+
+        # check whether stored file is the same as the matrix to preserve identical values when file is the source
+        #check_identity_of_img(output_filepath, lidar_bev)
+        #check_identity_of_img_float(output_filepath, lidar_bev)
+
+    print("\nSuccesfully transformed all '%s' lidar pointclouds" % chosen_dataset)
+
+
+if __name__ == "__main__":
+    img_height = 256
+    img_width = 512
+    dataset = "kitti"  # 'kitti', 'lyft' or 'nuscenes'
+    main(dataset, img_height, img_width)
---
+++ utils.py
@@ -0,0 +1,125 @@
+"""
+    Util scripts for building features, fetching ground truths, computing IoU, etc. 
+"""
+
+
+import numpy as np
+
+
+def removePoints(PointCloud, BoundaryCond):
+    # Boundary condition
+    minX = BoundaryCond['minX']
+    maxX = BoundaryCond['maxX']
+    minY = BoundaryCond['minY']
+    maxY = BoundaryCond['maxY']
+    minZ = BoundaryCond['minZ']
+    maxZ = BoundaryCond['maxZ']
+
+    # Remove the point out of range x,y,z
+    mask = np.where(
+            (PointCloud[:, 0] >= minX) &
+            (PointCloud[:, 0] <= maxX) &
+            (PointCloud[:, 1] >= minY) &
+            (PointCloud[:, 1] <= maxY) &
+            (PointCloud[:, 2] >= minZ) &
+            (PointCloud[:, 2] <= maxZ)
+            )
+    PointCloud = PointCloud[mask]
+    return PointCloud
+
+
+def makeBVFeature(PointCloud_, BoundaryCond, img_height, img_width, Discretization):
+    Height = img_height + 1
+    Width = img_width + 1
+
+    # Discretize Feature Map
+    PointCloud = np.copy(PointCloud_)
+    PointCloud[:, 0] = np.int_(np.floor(PointCloud[:, 0] / Discretization))
+    PointCloud[:, 1] = np.int_(np.floor(PointCloud[:, 1] / Discretization) + Width / 2)
+
+    # sort-3times
+    indices = np.lexsort((-PointCloud[:, 2], PointCloud[:, 1], PointCloud[:, 0]))
+    PointCloud = PointCloud[indices]
+
+    # Height Map
+    heightMap = np.zeros((Height, Width))
+
+    # because of the rounding of points, there are many identical points
+    _, indices = np.unique(PointCloud[:, 0:2], axis=0, return_index=True)
+    PointCloud_frac = PointCloud[indices]
+
+    # Some important problem is image coordinate is (y,x), not (x,y)
+    max_height = float(np.abs(BoundaryCond['maxZ'] - BoundaryCond['minZ']))
+    heightMap[np.int_(PointCloud_frac[:, 0]), np.int_(PointCloud_frac[:, 1])] = PointCloud_frac[:, 2] / max_height
+    #heightMap_normalized = (heightMap - BoundaryCond['minZ'])/abs(BoundaryCond['maxZ']-BoundaryCond['minZ'])  # Normalize to [0, 1]
+
+    # Intensity Map & DensityMap
+    intensityMap = np.zeros((Height, Width))
+    densityMap = np.zeros((Height, Width))
+
+    # 'counts': The number of times each of the unique values comes up in the original array
+    _, indices, counts = np.unique(PointCloud[:, 0:2], axis=0, return_index=True, return_counts=True)
+    PointCloud_top = PointCloud[indices]
+    normalizedCounts = np.minimum(1.0, np.log(counts + 1) / np.log(64))
+    intensityMap[np.int_(PointCloud_top[:, 0]), np.int_(PointCloud_top[:, 1])] = PointCloud_top[:, 3]
+    densityMap[np.int_(PointCloud_top[:, 0]), np.int_(PointCloud_top[:, 1])] = normalizedCounts
+
+    RGB_Map = np.zeros((Height-1, Width-1, 3))
+    RGB_Map[:, :, 0] = densityMap[0:img_height, 0:img_width]  # r_map
+    #RGB_Map[:, :, 1] = heightMap_normalized[0:img_height, 0:img_width]  # g_map
+    RGB_Map[:, :, 1] = heightMap[0:img_height, 0:img_width]  # g_map
+    RGB_Map[:, :, 2] = intensityMap[0:img_height, 0:img_width]  # / 255  # b_map
+
+    # normalize RGB_Map from [0, 1] to [0, 255]
+    #RGB_Map = (255 * RGB_Map).astype(np.uint8)
+
+    return RGB_Map
+
+
+def makeBVFeature_old(PointCloud_, BoundaryCond, img_height, img_width, Discretization):  # method origined of YOLO3D instead of ComplexYOLO
+    Height = img_width + 1
+    Width = img_width + 1
+
+    # Discretize Feature Map
+    PointCloud = np.copy(PointCloud_)
+    PointCloud[:, 0] = np.int_(np.floor(PointCloud[:, 0] / Discretization))
+    PointCloud[:, 1] = np.int_(np.floor(PointCloud[:, 1] / Discretization) + Width / 2)
+
+    # sort-3times
+    indices = np.lexsort((-PointCloud[:, 2], PointCloud[:, 1], PointCloud[:, 0]))
+    PointCloud = PointCloud[indices]
+
+    # Height Map
+    heightMap = np.zeros((Height, Width))
+
+    # because of the rounding of points, there are many identical points
+    _, indices = np.unique(PointCloud[:, 0:2], axis=0, return_index=True)
+    PointCloud_frac = PointCloud[indices]
+
+    # Some important problem is image coordinate is (y,x), not (x,y)
+    heightMap[np.int_(PointCloud_frac[:, 0]), np.int_(PointCloud_frac[:, 1])] = PointCloud_frac[:, 2]
+    #heightMap_normalized = (heightMap - BoundaryCond['minZ'])/abs(BoundaryCond['maxZ']-BoundaryCond['minZ'])  # Normalize to [0, 1]
+
+    # Intensity Map & DensityMap
+    intensityMap = np.zeros((Height, Width))
+    densityMap = np.zeros((Height, Width))
+
+    # 'counts': The number of times each of the unique values comes up in the original array
+    _, indices, counts = np.unique(PointCloud[:, 0:2], axis=0, return_index=True, return_counts=True)
+    PointCloud_top = PointCloud[indices]
+    normalizedCounts = np.minimum(1.0, np.log(counts + 1) / np.log(64))
+    intensityMap[np.int_(PointCloud_top[:, 0]), np.int_(PointCloud_top[:, 1])] = PointCloud_top[:, 3]
+    densityMap[np.int_(PointCloud_top[:, 0]), np.int_(PointCloud_top[:, 1])] = normalizedCounts
+    RGB_Map = np.zeros((Height, Width, 3))
+
+    # RGB channels respectively
+    RGB_Map[:, :, 0] = densityMap
+    #RGB_Map[:, :, 1] = heightMap_normalized
+    RGB_Map[:, :, 1] = heightMap_normalized
+    RGB_Map[:, :, 2] = intensityMap / 255
+
+    # normalize RGB_Map from [0, 1] to [0, 255]
+    #RGB_Map = (255 * RGB_Map).astype(np.uint8)
+
+    save = RGB_Map[0:img_height, 0:img_width, :]
+    return save
