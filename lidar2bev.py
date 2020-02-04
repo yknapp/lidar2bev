e47e8c57dad4a6ffd538f151c9cd470d1a6a4a25
Yannick Knapp <YaBaSa@web.de>
2020-02-04 15:39:56 +0000

    Add Audi's A2D2 dataset

--- lidar2bev.py
+++ lidar2bev.py
@@ -16,6 +16,8 @@ LYFT_KITTI_LIDAR_DIR = "/home/user/datasets/lyft_kitti/object/training/velodyne"
 LYFT_KITTI_OUTPUT_DIR = "/home/user/datasets/bev_images/lyft_kitti"
 NUSCENES_LIDAR_DIR = "/home/user/datasets/nuscenes/samples/LIDAR_TOP"
 NUSCENES_OUTPUT_DIR = "/home/user/datasets/bev_images/nuscenes/samples"
+AUDI_LIDAR_DIR = "/home/user/work/master_thesis/datasets/audi/camera_lidar_semantic_bboxes/20181108_103155/lidar/cam_front_center"
+AUDI_OUTPUT_DIR = "/home/user/work/master_thesis/datasets/bev_images/audi/"
 
 # lidar boundarys for Bird's Eye View
 BOUNDARY = {
@@ -61,6 +63,15 @@ def load_lidar_file_nuscenes(file_path):
     lidar_pc[:, 2] = pointcloud.points[2, :]
     lidar_pc[:, 3] = pointcloud.points[3, :]
 
+    return lidar_pc
+
+
+def load_lidar_file_audi(file_path):
+    lidar_pc_raw = np.load(file_path)
+    print(list(lidar_pc_raw.keys()))
+    lidar_pc = np.zeros([lidar_pc_raw['points'].shape[0], 4])
+    lidar_pc[:, :3] = lidar_pc_raw['points']
+    lidar_pc[:, 3] = lidar_pc_raw['reflectance']
     return lidar_pc
 
 
@@ -119,6 +130,10 @@ def main(chosen_dataset, img_height, img_width):
         load_lidar_file = load_lidar_file_kitti
         lidar_path = LYFT_KITTI_LIDAR_DIR
         output_dir = LYFT_KITTI_OUTPUT_DIR
+    elif chosen_dataset == "audi":
+        load_lidar_file = load_lidar_file_audi
+        lidar_path = AUDI_LIDAR_DIR
+        output_dir = AUDI_OUTPUT_DIR
     else:
         print("Error: Unknown dataset '%s'" % chosen_dataset)
         sys.exit()
@@ -144,7 +159,7 @@ def main(chosen_dataset, img_height, img_width):
         ########################
 
         # save as PNG image with the same image name like lidar file
-        output_filename = lidar_pc_filenames[idx].replace(".bin", ".png")
+        output_filename = lidar_pc_filenames[idx].replace(".bin", ".png").replace(".npz", ".png")
         output_filepath = os.path.join(output_dir, output_filename)
         imageio.imwrite(output_filepath, lidar_bev)
 
@@ -156,7 +171,8 @@ def main(chosen_dataset, img_height, img_width):
 
 
 if __name__ == "__main__":
-    img_height = 256
-    img_width = 512
-    dataset = "lyftkitti"  # 'kitti', 'lyft', 'nuscenes' or 'lyftkitti'
+    img_height = 418
+    img_width = 418
+    dataset = "audi"  # 'kitti', 'lyft', 'nuscenes', 'lyftkitti' or 'audi'
     main(dataset, img_height, img_width)
+
